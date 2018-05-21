6a7d577e5f0fa8a6ebc458e27858b1939b5c80bf
SergioEric <sericmorales@gmail.com>
2018-05-21 15:00:32 -0500

    added page move

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { MovePage } from '../pages/move/move';
 import { ClientPage } from '../pages/clients/clients';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -38,7 +38,7 @@ const config = {
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    MovePage,
     ClientPage,
     HomePage,
     TabsPage,
@@ -57,7 +57,7 @@ const config = {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    MovePage,
     ClientPage,
     HomePage,
     TabsPage,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
+import { MovePage } from '../move/move';
 import { ClientPage } from '../clients/clients';
 import { HomePage } from '../home/home';
 
@@ -10,7 +10,7 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
+  tab2Root = MovePage;
   tab3Root = ClientPage;
 
   constructor() {
